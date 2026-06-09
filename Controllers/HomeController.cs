7002ae1826f5e5df02f906bc2f38eb63dc9f832e
LUAN JUSTINIANO DOS SANTOS <luanj12183@gmail.com>
2026-06-09 22:40:48 +0000

    Atualização

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,12 +15,12 @@ public class HomeController : Controller
     [HttpPost]
     public IActionResult Buscar(EnderecoViewModel model)
     {
-        if (!ModelState.IsValid)
-        {
-            return View(model);
-        }
+       // if (!ModelState.IsValid)
+        //{
+         //   return View(model);
+        //}
 
-        return RedirectToAction("Resultado", model);
+        return View ("Resultado", model);
     }
 
     [HttpGet]
